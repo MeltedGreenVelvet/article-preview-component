a286fde715dce46ccf7c85e62f70f86ef6cfe670
MeltedGreenVelvet <89157503+MeltedGreenVelvet@users.noreply.github.com>
2023-05-30 12:52:45 -0400

    Desktop Responsivity
    - Updated the js script to fix wonky transitions that didn't translate responsively.
    - Restyled the card footer to account for the change of design when going from mobile to desktop.
    - Created a CSS .down-arrow for the desktop design.

--- script.js
+++ script.js
@@ -2,13 +2,34 @@ const cardAuthor = document.querySelector('.card-author');
 const cardFooter = document.querySelector('.card-footer');
 const cardShareBtn = document.querySelector('.card-share-btn');
 const cardShareBackBtn = document.querySelector('.card-share-back-btn');
+const minWidth = 1000;
+
+function updateOpacity () {
+    if (window.innerWidth >= minWidth) {
+        cardAuthor.style.opacity = '1';
+        cardShareBtn.style.opacity = '0';
+        cardShareBackBtn.style.transition = 'none';
+    }else{
+        cardAuthor.style.opacity = '0';
+    }
+}
 
 cardShareBtn.addEventListener('click', () => {
-    cardAuthor.style.opacity = '0';
+    updateOpacity();
     cardFooter.classList.add('visible');
 });
 
 cardShareBackBtn.addEventListener('click', () => {
     cardAuthor.style.opacity = '1';
+    cardShareBtn.style.opacity = '1';
     cardFooter.classList.remove('visible');
+});
+
+window.addEventListener('resize', function(){
+    updateOpacity();
+    if (window.innerWidth >= minWidth) {
+        cardShareBtn.style.opacity = '1';
+    }else {
+        cardAuthor.style.opacity = '1';
+    }
 });
